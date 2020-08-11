41ab8beca04b42526bc57f022dcd369bec6018f3
Almir <almir.tihak98@hotmail.com>
2020-08-11 03:05:51 +0200

    Init

---
+++ SmartGreenhouse/SmartGreenhouse/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SmartGreenhouse.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
